e9d81588e051f115f742cd96223052b9ba73d10b
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2020-11-24 11:57:51 +0800

    點擊判定

---
+++ AR_foundation/Assets/ARManager.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;  //引用 Foundation API
+using System.Collections.Generic;  //引用 系統.一班 包含清單 List
+
+
+
+//RC 要求元件：在第一次套用腳本時執行
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARManager : MonoBehaviour
+{
+    [Header("點擊後要生成的物件")]
+    public GameObject obj;
+    [Header("AR管理器")]
+    public ARRaycastManager arManager;
+
+    //滑鼠座標
+    private Vector2 pointMouse;
+    //碰撞資訊
+    private List<ARRaycastHit> hits;
+
+    //<summary?>
+    //點擊地面生成物件
+    //</summary>
+    private void Tap()
+    {
+        //判斷玩家是否點擊
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointMouse = Input.mousePosition;    //滑鼠座標=玩家的滑鼠座標
+            print(pointMouse);  //輸出座標測試用
+        }
+        //判斷設限是否打到物件
+        //生成物件
+    }
+    private void Update()
+    {
+        Tap();
+    }
+}
